4436a4122df5de464f714829b2dfe8db7e647937
amitshakarchy <58327011+amitshakarchy@users.noreply.github.com>
2022-05-22 08:54:52 +0300

    added files

--- image_cropper.py
+++ image_cropper.py
@@ -1,1 +1,16 @@
+def single_nuc_crop(x, y, win_size, spot_frame, video):
+    """
+    The method returns a cropped image of the given nuclei's information
+    :param x: nuclei's center x coordination
+    :param y: nuclei's center y coordination
+    :param win_size: diameter of the cropped image (image size is win_size X win_size)
+    :param spot_frame: frame number to crop an image from
+    :param video: nuclei channel video
+    :return: single_nuc_crop: np.array of shape (win_size, win_size, 1) of the cropped image
+    """
+    single_nuc_crop = None
+    # TODO: your code here...
+
+    return single_nuc_crop
+
 
--- segment_image.py
+++ segment_image.py
@@ -1,16 +1,22 @@
 def preprocess_image(img):
     """
-
+    Preprocessing of a single image
+    :param img: input image
+    :return: reshaped image (if more preprocessing is needed, add it here!)
     """
     processes_image = None
+    # TODO: your code here...
 
     return processes_image
 
 
 def segment_image(img):
     """
-
+    Segmentation of a single image
+    :param img: preprocessed image
+    :return: seg_image- ndArray of the segmentation
     """
     seg_image = None
-
+    # TODO: your code here...
+    
     return seg_image

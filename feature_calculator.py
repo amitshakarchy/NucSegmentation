742581d3e2b7965196d2c1a7ad9cb6920b39632f
amitshakarchy <58327011+amitshakarchy@users.noreply.github.com>
2022-05-22 08:41:19 +0300

    Initial commit

---
+++ feature_calculator.py
@@ -0,0 +1,39 @@
+class NucleiFeatureCalculator:
+
+    @staticmethod
+    def aspect_ratio(seg_img):
+        """
+        calculates the aspect ratio of a single nuclei
+        :param seg_img: segmented image
+        :return: aspect_ratio
+        """
+        aspect_ratio = None
+        # TODO: your code here...
+
+        return aspect_ratio
+
+    @staticmethod
+    def size(seg_img):
+        """
+        calculates the size of a single nuclei
+        :param seg_img: segmented image
+        :return: size
+        """
+        size = None
+        # TODO: your code here...
+
+        return size
+
+    @staticmethod
+    def intensity(seg_img, original_img):
+        """
+        calculates min, max, mean, std and sum of the nuclei's intensity
+        :param
+            seg_img: segmented image
+            original_img: the original image
+        :return: int_min, int_max, int_mean, int_std, int_sum
+        """
+        int_min, int_max, int_mean, int_std, int_sum = None, None, None, None, None
+        # TODO: your code here...
+
+        return int_min, int_max, int_mean, int_std, int_sum

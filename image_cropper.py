2959c2b8293835a08e2c02c1ca444918cf117590
reut mealem <rm33334@gmail.com>
2022-06-09 11:58:01 +0300

    crop_cell and segmentation

--- image_cropper.py
+++ image_cropper.py
@@ -1,3 +1,9 @@
+
+import skimage.io
+import numpy as np
+from utils import get_tracks
+import matplotlib.pyplot as plt
+
 def single_nuc_crop(x, y, win_size, spot_frame, video):
     """
     The method returns a cropped image of the given nuclei's information
@@ -8,12 +14,36 @@ def single_nuc_crop(x, y, win_size, spot_frame, video):
     :param video: nuclei channel video
     :return: single_nuc_crop: np.array of shape (win_size, win_size, 1) of the cropped image
     """
-    single_nuc_crop = None
-    # TODO: your code here...
+    single_nuc_crop = np.zeros((win_size, win_size), )
+    video_tif = skimage.io.imread(video)
+    video_np = np.array(video_tif)
+
+    # Crop the needed nuclear
+    single_nuc_crop = video_np[spot_frame,y-int(win_size/2):y+int(win_size/2), x-int(win_size/2):x+int(win_size/2)]
 
     return single_nuc_crop
 
 
 if __name__ == '__main__':
-    print()
-#dlfghkfdklghlfdhgldlglfdhl
+    excel_PWD = "C:\\Users\\reut mealem\\Desktop\\FeatureAndTagTable-vertices.csv"
+    video_PWD = "C:\\Users\\reut mealem\\Desktop\\211212erki-p38-stiching_s2_tdTom_ORG.tif"
+    cells_table, unrelevent = get_tracks(excel_PWD)
+    df2 = cells_table[['Spot position X (µm)', 'Spot position Y (µm)', 'Spot frame']]
+
+    # x = df2.iat[0,0]
+    # y = df2.iat[0,1]
+    # spot_frame = int(df2.iat[0,2])
+
+    x = df2.iat[2, 0]
+    y = df2.iat[2, 1]
+    spot_frame = int(df2.iat[2, 2])
+    #convert from coordinates to location in the image
+
+    x = int(x/0.462)
+    y = int(y/0.462)
+
+    abc = single_nuc_crop(x,y,50,spot_frame,video_PWD)
+    plt.imshow(abc)
+    plt.imsave("C:\\Users\\reut mealem\\PycharmProjects\\NucSegmentation\\resources\\many.png", abc)
+    plt.show()
+
--- segment_image.py
+++ segment_image.py
@@ -1,11 +1,22 @@
+import numpy as np
+from deepcell.applications import NuclearSegmentation
+import skimage.io
+from utils import get_tracks
+import matplotlib.pyplot as plt
+from image_cropper import single_nuc_crop
+from skimage.io import imread
+
+
 def preprocess_image(img):
     """
     Preprocessing of a single image
     :param img: input image
     :return: reshaped image (if more preprocessing is needed, add it here!)
     """
-    processes_image = None
-    # TODO: your code here...
+
+    im = np.expand_dims(img, axis=-1)
+    im = np.expand_dims(im, axis=0)
+    processes_image = im
 
     return processes_image
 
@@ -16,7 +27,38 @@ def segment_image(img):
     :param img: preprocessed image
     :return: seg_image- ndArray of the segmentation
     """
-    seg_image = None
-    # TODO: your code here...
-    
+    app = NuclearSegmentation()
+
+    # create the lab
+    seg_image = app.predict(img)
+
     return seg_image
+
+if __name__ == '__main__':
+    excel_PWD = "C:\\Users\\reut mealem\\Desktop\\FeatureAndTagTable-vertices.csv"
+    video_PWD = "C:\\Users\\reut mealem\\Desktop\\211212erki-p38-stiching_s2_tdTom_ORG.tif"
+    cells_table, unrelevent = get_tracks(excel_PWD)
+    df2 = cells_table[['Spot position X (µm)', 'Spot position Y (µm)', 'Spot frame']]
+
+    x = df2.iat[2, 0]
+    y = df2.iat[2, 1]
+    spot_frame = int(df2.iat[2, 2])
+    #convert from coordinates to location in the image
+
+    # x = int(x/0.462)
+    # y = int(y/0.462)
+    #
+    # abc = single_nuc_crop(x,y,50,spot_frame,video_PWD)
+    # plt.imshow(abc)
+    # plt.show()
+    #
+    # plt.imsave("C:\\Users\\reut mealem\\PycharmProjects\\NucSegmentation\\resources\\manymany.png", abc)
+    # skimage.io.imsave("C:\\Users\\reut mealem\\PycharmProjects\\NucSegmentation\\resources\\many.tif", abc,
+    #                   check_contrast=False)
+    img = imread('resources/manymany.png', as_gray=True)
+    # img = imread('resources/many.tif', as_gray=True)
+
+    preProcess = preprocess_image(img)
+    seg = segment_image(preProcess)
+    plt.imshow(seg.reshape(50,50,1))
+    plt.show()
